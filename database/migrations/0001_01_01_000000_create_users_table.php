c0abd85c475d60b667b95968790ab5ba32b00dd4
Your Name <you@example.com>
2025-02-25 19:30:21 +0600

    up

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('roll');
+            $table->string('roll')->default('customer');
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
         });
